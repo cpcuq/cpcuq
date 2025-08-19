ef62bc68adac6d1de9687dd2ceb31aa989a65d20
cpcuq <cpcuniquindio@gmail.com>
2025-08-19 15:00:01 -0500

    feat: add basic I/O info and syncing

--- basic.cpp
+++ basic.cpp
@@ -2,5 +2,14 @@
 using namespace std;
 
 int main() {
-  
+  ios_base::sync_with_stdio(false);
+  cin.tie(nullptr);
+  //cin>>x;
+  /*scanf("%d",&w);
+  //printf("\n");*/
+  /*
+  string s;
+  while(getline(cin, s)) {}
+
+  */
 }
